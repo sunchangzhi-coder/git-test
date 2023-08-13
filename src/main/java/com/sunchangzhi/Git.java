2f6737a29baa97936176748b587ae79e827e8182
sunchangzhi <3488531241@qq.com>
2023-08-13 18:02:38 +0800

    first commit!

--- src/main/java/com/sunchangzhi/Git.java
+++ src/main/java/com/sunchangzhi/Git.java
@@ -3,5 +3,6 @@ package com.sunchangzhi;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
+        System.out.println("Hello, Git second!");
     }
 }
