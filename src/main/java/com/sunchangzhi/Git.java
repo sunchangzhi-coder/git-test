8cde94d196c919d56be8d5bf7fbbd5021ea942e9
sunchangzhi <3488531241@qq.com>
2023-08-13 18:22:29 +0800

    hot-fix commit1

--- src/main/java/com/sunchangzhi/Git.java
+++ src/main/java/com/sunchangzhi/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Hello, Git second!");
+        System.out.println("Hot fix branch code");
     }
 }
